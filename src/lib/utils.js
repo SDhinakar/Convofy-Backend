2cc27342816a88c9a592cebdce31ff64e73259df
Dhinakar S <dhinakarshanmugam6@gmail.com>
2025-06-20 07:57:45 +0530

    error r1

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,14 +1,16 @@
 import jwt from 'jsonwebtoken';
 
-export const generateToken = (userId,res) => {
-    const token = jwt.sign({userId},process.env.JWT_SECRET,{
-        expiresIn: '7d'
-    })
-    res.cookie("jwt",token,{
-        maxAge : 7 * 24 * 60 * 60 * 1000, // 7 days
-        httpOnly: true, // Prevents client-side JavaScript from accessing the cookie
-        sameSite : 'strict', // Helps prevent CSRF attacks
-        secure : process.env.NODE_ENV === 'production' // Use secure cookies in production
-    });
-    return token;
-}
+export const generateToken = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: '7d'
+  });
+
+  res.cookie('jwt', token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    httpOnly: true,
+    sameSite: 'None',
+    secure: true // Always true for cross-origin with HTTPS
+  });
+
+  return token;
+};
