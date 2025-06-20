78064c1da37c8b224dd9c12e7909cecc4439e52f
Dhinakar S <dhinakarshanmugam6@gmail.com>
2025-06-20 08:03:44 +0530

    error r2

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -8,6 +8,7 @@ export function setupSocket(server) {
   io = new Server(server, {
     cors: {
       origin: process.env.NODE_ENV === "production" ? process.env.CLIENT_URL : "http://localhost:5173",
+      credentials: true,
       methods: ["GET", "POST", "PUT", "DELETE"],
     },
   });
